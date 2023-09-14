cac3c5acec4343e6b5a145fd8aaae1b158da5ba8
Kanata Miyahana <afiri.bupa@gmail.com>
2023-09-14 23:38:57 +0900

    update ast/main.go

--- ast/main.go
+++ ast/main.go
@@ -2,25 +2,47 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
 	"go/parser"
 	"go/token"
-	"reflect"
+	"log"
 )
 
 func main() {
 	const src = `package main
-import "fmt" // GenDeclとして解析されるはず
-
-func main() {} // FuncDeclとして解析されるはず
-
-fun hoge( // BadDeclとして解析されるはず
+func main() {
+;
+	goto HOGE
+	HOGE:
+}
 `
 
 	fset := token.NewFileSet()
 
-	// BadDeclの例を見るためにエラーは無視
-	f, _ := parser.ParseFile(fset, "", src, parser.Mode(0))
+	f, err := parser.ParseFile(fset, "", src, parser.Mode(0))
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 	for _, d := range f.Decls {
-		fmt.Println(reflect.TypeOf(d))
+		fun, ok := d.(*ast.FuncDecl)
+		if !ok {
+			continue
+		}
+
+		funcBody := fun.Body
+		for _, stmt := range funcBody.List {
+			emptyStmt, ok := stmt.(*ast.EmptyStmt)
+			if ok {
+				fmt.Printf("found EmptyStmt at %v\n", fset.Position(emptyStmt.Pos()))
+				ast.Print(fset, emptyStmt)
+			}
+
+			labeledStmt, ok := stmt.(*ast.LabeledStmt)
+			if ok {
+				fmt.Printf("found LabeledStmt at %v\n", fset.Position(labeledStmt.Pos()))
+				ast.Print(fset, labeledStmt)
+			}
+		}
 	}
 }
