f452235ba7a692deecd1cfe27f25ddd091166c2f
Kanata Miyahana <kanata-miyahana@KanatanoMacBook-Pro.local>
2024-11-10 22:30:56 +0900

    add scope coe

--- types/main.go
+++ types/main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/k3forx/go-static-analysis/types/identifier_resolution"
 	"github.com/k3forx/go-static-analysis/types/info_defs"
 	"github.com/k3forx/go-static-analysis/types/info_uses"
 	pkg "github.com/k3forx/go-static-analysis/types/package"
+	"github.com/k3forx/go-static-analysis/types/scope"
 )
 
 func main() {
+	printDivider("Identifier Resolution")
 	identifier_resolution.Do()
-	printDivider()
 
+	printDivider("Info.Defs")
 	info_defs.Do()
-	printDivider()
 
+	printDivider("Info.Uses")
 	info_uses.Do()
-	printDivider()
 
+	printDivider("Package")
 	pkg.Do()
-	printDivider()
+
+	printDivider("Scope")
+	scope.Do()
 }
 
-func printDivider() {
-	println("-------------------------------------------------")
+func printDivider(name string) {
+	fmt.Printf("---------------------- %s --------------------------\n", name)
 }
---
+++ types/scope/scope.go
@@ -0,0 +1,53 @@
+package scope
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"log"
+)
+
+const pkgStr = `package main
+
+var a int
+
+func main() {
+	b := "hello"
+	_ = b
+}
+`
+
+func Do() {
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "main.go", pkgStr, parser.Mode(0))
+	if err != nil {
+		log.Fatalf("parsing file: %v", err)
+	}
+
+	conf := types.Config{}
+	pkgInfo, err := conf.Check("main.go", fset, []*ast.File{f}, nil)
+	if err != nil {
+		log.Fatalf("checking types: %v", err)
+	}
+
+	fmt.Printf("Name: %s\n", pkgInfo.Name())
+	fmt.Printf("# of scope in %s: %+v\n", pkgInfo.Name(), pkgInfo.Scope().NumChildren())
+
+	for _, name := range pkgInfo.Scope().Names() {
+		obj := pkgInfo.Scope().Lookup(name)
+		switch tp := obj.(type) {
+		case *types.Var:
+			fmt.Printf("scope: %+v\n", tp.String())
+		}
+	}
+
+	for i := range pkgInfo.Scope().NumChildren() {
+		child := pkgInfo.Scope().Child(i)
+		for _, name := range child.Names() {
+			obj := child.Lookup(name)
+			fmt.Printf("child obj: %+v\n", obj)
+		}
+	}
+}
